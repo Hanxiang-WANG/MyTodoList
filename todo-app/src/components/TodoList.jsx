4f10c9db091fe569aafbdc3477c817bd9fdf0da3
Hanxiang-WANG <hanxiangw@student.unimelb.edu.au>
2024-10-03 22:26:21 +1000

    updated todolist component

--- todo-app/src/components/TodoList.jsx
+++ todo-app/src/components/TodoList.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-TodoList = () => {
+function TodoList() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
   return (
-    <div>TodoList</div>
+    <div className='max-w-md mx-auto m-10 p-4 bg-white rounded-lg shadow-lg'>
+        <h1 className='mb-4 text-2xl font-bold'>My Todo List</h1>
+        <div className='flex mb-4'>
+            <input 
+                type='text'
+                className='flex-1 p-2 border rounded'
+                value={newTask}
+                onChange={(e) => setNewTask(e.target.value)}
+                placeholder='Add a new task...'
+            />
+        </div>
+    </div>
   )
 }
 
