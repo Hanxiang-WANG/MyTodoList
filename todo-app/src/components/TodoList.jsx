7886fae7921a878f0e0b17bd58303ad7edea8792
Hanxiang-WANG <hanxiangw@student.unimelb.edu.au>
2024-10-03 23:04:20 +1000

    updated crud functions

--- todo-app/src/components/TodoList.jsx
+++ todo-app/src/components/TodoList.jsx
@@ -4,6 +4,24 @@ function TodoList() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
 
+  const addTask = () => {
+    if(newTask.trim()){
+        setTasks([...tasks, { text: newTask, completed: false }]);
+        setNewTask('');
+    }
+  };
+
+  const toggleTaskCompletion = (index) => {
+    const updatedTasks = tasks.map((task, i) => 
+    i === index ? { ...task, completed: !task.completed } : task);
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className='max-w-md mx-auto m-10 p-4 bg-white rounded-lg shadow-lg'>
         <h1 className='mb-4 text-2xl font-bold'>My Todo List</h1>
